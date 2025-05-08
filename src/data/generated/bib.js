ebbdba1e95d44663dc1383c758da8cb62eeca6b6
g1051083853 <1051083853@qq.com>
2025-05-08 12:59:53 +0800

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,117 @@
+﻿define({ entries : {
+    "bodnar2024foundation": {
+        "archiveprefix": "arXiv",
+        "author": "Bodnar, C. and Bruinsma, W. P. and Lucic, A. and others",
+        "eprint": "2405.13063",
+        "keywords": " domain:climate-modeling, method:ai-foundation-models, ml:transformer",
+        "title": "A foundation model for the earth system",
+        "type": "article",
+        "year": "2024"
+    },
+    "guo2019ambient": {
+        "author": "Guo, Y. and Teixeira, J. P. and Ryti, N. R. I.",
+        "doi": "10.1056/NEJMoa1817364",
+        "journal": "New England Journal of Medicine",
+        "keywords": " domain:urban-health, method:multi-city-study, dataset:urban-mortality",
+        "number": "8",
+        "pages": "705--715",
+        "title": "Ambient particulate air pollution and daily mortality in 652 cities",
+        "type": "article",
+        "volume": "381",
+        "year": "2019"
+    },
+    "he2021spatiotemporal": {
+        "author": "He, Zhen-fang and Guo, Qing-chun and Liu, Jia-zhen and Zhang, Ying-ying and Liu, Jie and Ding, Hang",
+        "doi": "10.31497/zrzyxb.20210211",
+        "journal": "Journal of Natural Resources",
+        "keywords": " domain:geospatial-analysis, method:gis-modeling, dataset:hebei-pollution",
+        "number": "2",
+        "pages": "411--419",
+        "title": "Spatio-temporal variation characteristics of air pollution and influencing factors in Hebei province",
+        "type": "article",
+        "volume": "36",
+        "year": "2021"
+    },
+    "lelieveld2015contribution": {
+        "author": "Lelieveld, J. and Evans, J. and Fnais, M. and others",
+        "doi": "10.1038/nature15371",
+        "journal": "Nature",
+        "keywords": " domain:public-health, method:epidemiological-study, ml:risk-modeling",
+        "pages": "367--371",
+        "title": "The contribution of outdoor air pollution sources to premature mortality on a global scale",
+        "type": "article",
+        "volume": "525",
+        "year": "2015"
+    },
+    "li2023deep": {
+        "archiveprefix": "arXiv",
+        "author": "Li, X. and Chen, M. and Wang, H.",
+        "eprint": "2504.19764",
+        "keywords": " domain:urban-planning, method:real-time-prediction, ml:multimodal-learning",
+        "title": "Deep learning-based real-time prediction of urban air quality using multimodal data fusion",
+        "type": "article",
+        "year": "2023"
+    },
+    "li2024analysis": {
+        "author": "Li, X. Y. and Wang, A. A.",
+        "doi": "10.12677/ojns.2024.122048",
+        "journal": "Natural Sciences",
+        "keywords": " domain:environmental-monitoring, method:time-series-analysis, dataset:tianjin-aq",
+        "number": "2",
+        "pages": "412--425",
+        "title": "Analysis of air quality variation characteristics and influencing factors in Tianjin over the past five years",
+        "type": "article",
+        "volume": "12",
+        "year": "2024"
+    },
+    "roser2021data": {
+        "author": "Roser, Max",
+        "keywords": " domain:environment, method:meta-analysis, dataset:global-pollution",
+        "title": "Data review: How many people die from air pollution?",
+        "type": "online",
+        "url": "https://ourworldindata.org/data-review-air-pollution-deaths",
+        "year": "2021"
+    },
+    "wang2020joint": {
+        "author": "Wang, C. and Han, L.",
+        "doi": "10.12677/AEP.2020.105090",
+        "journal": "Advances in Environmental Protection",
+        "keywords": " domain:climate-studies, method:multivariate-analysis, dataset:sichuan-weather",
+        "number": "5",
+        "pages": "745--754",
+        "title": "Joint analysis of air pollutant concentrations and weather factors in Sichuan Province over the past three years",
+        "type": "article",
+        "volume": "10",
+        "year": "2020"
+    },
+    "wu2022impacts": {
+        "author": "Wu, J. and Bei, N. and Li, X. and others",
+        "doi": "10.1029/2021JD036213",
+        "journal": "Journal of Geophysical Research: Atmospheres",
+        "keywords": " domain:atmospheric-science, method:transport-modeling, dataset:coastal-china",
+        "title": "Impacts of transboundary transport on coastal air quality of South China",
+        "type": "article",
+        "volume": "127",
+        "year": "2022"
+    },
+    "xie2022deep": {
+        "author": "Xie, F. and Yuan, H. and Ning, Y. and others",
+        "doi": "10.1016/j.jbi.2021.103980",
+        "journal": "Journal of Biomedical Informatics",
+        "keywords": " domain:medical-informatics, method:systematic-review, ml:time-series-embedding",
+        "pages": "103980",
+        "title": "Deep learning for temporal data representation in electronic health records: A systematic review",
+        "type": "article",
+        "volume": "126",
+        "year": "2022"
+    },
+    "yao2021air": {
+        "author": "Yao, D. and Chen, X. and Li, E. and others",
+        "doi": "10.1016/j.scitotenv.2021.145402",
+        "journal": "Science of The Total Environment",
+        "keywords": " domain:environmental-chemistry, method:chemical-analysis, dataset:yangtze-delta",
+        "title": "Air pollution increases human health risks of PM2.5-bound PAHs and nitro-PAHs: A case study in the Yangtze River Delta, China",
+        "type": "article",
+        "year": "2021"
+    }
+}});
